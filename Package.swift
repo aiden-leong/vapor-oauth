5af285e896c7c2d8c9c41fbf678d64dc70e84899
Aiden Leong <1247008998@qq.com>
2021-01-20 15:54:57 +0800

    set platform to macOS 10.15

--- Package.swift
+++ Package.swift
@@ -4,12 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "VaporOAuth",
-    products: [
-        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
+    platforms: [
+        .macOS(.v10_15)
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
 //        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0")),
+    ],
+    products: [
+        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
     ],
     targets: [
         .target(name: "VaporOAuth", dependencies: ["Vapor"]),
