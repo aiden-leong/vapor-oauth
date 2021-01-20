5424738878e6550a08aa835d8470e18f4be2b934
ailion <1247008998@qq.com>
2021-01-20 16:27:01 +0800

    add products

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ let package = Package(
     platforms: [
        .macOS(.v10_15)
     ],
+    products: [
+        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
