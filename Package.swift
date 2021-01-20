c9f335eeba4cad118eedc06164dbd4b22cc86e34
ailion <1247008998@qq.com>
2021-01-20 16:19:49 +0800

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
 // swift-tools-version:5.2
-
 import PackageDescription
 
 let package = Package(
     name: "VaporOAuth",
     platforms: [
-        .macOS(.v10_15)
-    ],
-    products: [
-        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
+       .macOS(.v10_15)
     ],
     dependencies: [
+        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-//        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0")),
     ],
     targets: [
-        .target(name: "VaporOAuth", dependencies: ["Vapor"]),
+        .target(
+            name: "VaporOAuth",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        )
     ]
 )
