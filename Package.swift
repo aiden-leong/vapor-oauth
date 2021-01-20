5b347f41110e7bf8435743667bcd1015ce510331
ailion <1247008998@qq.com>
2021-01-20 15:59:34 +0800

    set swift-tools-version to 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -7,12 +7,12 @@ let package = Package(
     platforms: [
         .macOS(.v10_15)
     ],
+    products: [
+        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
 //        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.2.0")),
-    ],
-    products: [
-        .library(name: "VaporOAuth", targets: ["VaporOAuth"]),
     ],
     targets: [
         .target(name: "VaporOAuth", dependencies: ["Vapor"]),
